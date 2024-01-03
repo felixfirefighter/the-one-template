2eed6bf93e35c19b5e2b2376dd4ccc5bff26fb3b
Felix Lee <leeyoongti@gmail.com>
2024-01-03 20:35:06 +0800

    feat(dashboard): add rechart bar chart

--- apps/web/components/dashboard/overview-chart/index.tsx
+++ apps/web/components/dashboard/overview-chart/index.tsx
@@ -1,14 +1,20 @@
+'use client'
+
+import { Paper, useMantineTheme } from '@mantine/core'
+import { AppText } from '@the-one/ui'
 import { Bar, BarChart, ResponsiveContainer } from 'recharts'
 import { DATA } from './types'
 
 export const OverviewChart = () => {
+  const theme = useMantineTheme()
   return (
-    <div style={{ width: '100%' }}>
-      <ResponsiveContainer width="500px" height="500px">
-        <BarChart width={200} height={400} data={DATA}>
-          <Bar dataKey="pv" fill="#000" />
+    <Paper radius={'lg'} shadow="sm" p={24} withBorder>
+      <AppText fontWeight='500'>Overview</AppText>
+      <ResponsiveContainer width={'100%'} height={500}>
+        <BarChart data={DATA}>
+          <Bar dataKey="value" fill={theme.colors.dark[7]} />
         </BarChart>
       </ResponsiveContainer>
-    </div>
+    </Paper>
   )
 }
--- apps/web/components/dashboard/overview-chart/types.ts
+++ apps/web/components/dashboard/overview-chart/types.ts
@@ -1,44 +1,30 @@
 export const DATA = [
   {
     name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
+    value: 1000,
   },
   {
     name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
+    value: 1000,
   },
   {
     name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
+    value: 1000,
   },
   {
     name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
+    value: 1000,
   },
   {
     name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
+    value: 1000,
   },
   {
     name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
+    value: 1000,
   },
   {
     name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
+    value: 1000,
   },
 ]
