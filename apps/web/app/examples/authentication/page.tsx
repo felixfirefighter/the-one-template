fd1258cf48a127e844b83d12c5fe6ba40c93a4d1
Felix Lee <leeyoongti@gmail.com>
2024-01-12 20:25:58 +0800

    feat(authentication): add base template

--- apps/web/app/examples/authentication/page.tsx
+++ apps/web/app/examples/authentication/page.tsx
@@ -1,10 +1,30 @@
-import { Container } from "@mantine/core";
+import { Box, Container, Flex } from '@mantine/core'
+import { IconTriangle } from '@tabler/icons-react'
+import { AppSpacing, AppText } from '@the-one/ui'
 
 export default function Page(): JSX.Element {
-    return (
-      <Container>
-        Hello Authentication
-      </Container>
-    )
-  }
-  
+  return (
+    <Container fluid p={0}>
+      <Flex>
+        <Box
+          flex={1}
+          bg={'dark'}
+          c="gray.2"
+          p="xl"
+          style={{
+            borderTopLeftRadius: AppSpacing['3xl'],
+            borderBottomLeftRadius: AppSpacing['3xl'],
+          }}
+        >
+          <Flex>
+            <Box mr={'xs'}>
+              <IconTriangle />
+            </Box>
+            <AppText>The One Inc</AppText>
+          </Flex>
+        </Box>
+        <Box flex={1}></Box>
+      </Flex>
+    </Container>
+  )
+}
