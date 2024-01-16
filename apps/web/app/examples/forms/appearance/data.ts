be5fb9b255191b6eedd897027650f6ddd9d7619b
Felix Lee <leeyoongti@gmail.com>
2024-01-16 22:11:10 +0800

    feat(forms): add apperance page

---
+++ apps/web/app/examples/forms/appearance/data.ts
@@ -0,0 +1,20 @@
+
+
+export const FONTS = [
+  {
+    label: 'Inter',
+    value: 'inter',
+  },
+  {
+    label: 'Lato',
+    value: 'lato',
+  },
+  {
+    label: 'Open Sans',
+    value: 'open-sans',
+  },
+  {
+    label: 'Roboto',
+    value: 'roboto'
+  }
+]
--- apps/web/app/examples/forms/appearance/page.tsx
+++ apps/web/app/examples/forms/appearance/page.tsx
@@ -1,9 +1,39 @@
-import { Container } from '@mantine/core'
+'use client'
+
+import { Box, Button, Container, Divider, Select } from '@mantine/core'
+import { AppText } from '@the-one/ui'
+import { useState } from 'react'
+import { FONTS } from './data'
 
 export default function Page(): JSX.Element {
+  const [font, setFont] = useState('inter')
+
   return (
-    <Container size={'xl'}>
-      Profile
+    <Container size={'sm'} m={0}>
+      <AppText size="lg" fontWeight="600">
+        Appearance
+      </AppText>
+      <AppText c="gray.6" size="sm">
+        Customize the appearance of the app including font and theme.
+      </AppText>
+
+      <Divider my={'lg'}></Divider>
+
+      <Select
+        data={FONTS}
+        label="Font"
+        description="Set the font you want to use in the dashboard"
+        value={font}
+        onChange={(value) => {
+          if (value) {
+            setFont(value)
+          }
+        }}
+      ></Select>
+
+      <Box py={'xl'}>
+        <Button>Update appearance</Button>
+      </Box>
     </Container>
   )
 }
