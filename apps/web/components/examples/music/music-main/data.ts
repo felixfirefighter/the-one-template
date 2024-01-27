00af85f20d21934560816d9f572061a600ad1404
Felix Lee <leeyoongti@gmail.com>
2024-01-27 16:12:19 +0800

    feat(music): add main cards

--- apps/web/components/examples/music/music-main/data.ts
+++ apps/web/components/examples/music/music-main/data.ts
@@ -10,3 +10,26 @@ export const CONTROLS = [
     value: 'podcasts'
   }
 ]
+
+export const MAIN_MUSICS = [
+  {
+    image: '/music/music-1.jpg',
+    title: 'Song of Mine',
+    name: 'Ethan Winters'
+  },
+  {
+    image: '/music/music-2.jpg',
+    title: 'Godfather',
+    name: 'Robert Garcia'
+  },
+  {
+    image: '/music/music-3.jpg',
+    title: 'The Awakenings',
+    name: 'Brendon Brown'
+  },
+  {
+    image: '/music/music-4.jpg',
+    title: 'Kickstart My Heart',
+    name: 'Match Order'
+  }
+]
--- apps/web/components/examples/music/music-main/index.tsx
+++ apps/web/components/examples/music/music-main/index.tsx
@@ -1,15 +1,60 @@
-import { Box, Button, Flex, SegmentedControl } from '@mantine/core'
-import { CONTROLS } from './data'
+'use client'
+
+import {
+  Box,
+  Button,
+  Flex,
+  Image,
+  Paper,
+  SegmentedControl,
+} from '@mantine/core'
 import { IconPlus } from '@tabler/icons-react'
-import { AppSize } from '@the-one/ui'
+import { AppSize, AppText } from '@the-one/ui'
+import { CONTROLS, MAIN_MUSICS } from './data'
+import styles from './index.module.css'
 
 export const MusicMain = () => {
   return (
-    <Box p={'lg'} flex={1}>
-      <Flex justify={'space-between'}>
+    <Box p={'xl'} flex={1}>
+      <Flex justify={'space-between'} mb="lg">
         <SegmentedControl data={CONTROLS}></SegmentedControl>
-        <Button leftSection={<IconPlus size={AppSize['4xl']} />}>Add music</Button>
+        <Button leftSection={<IconPlus size={AppSize['4xl']} />}>
+          Add music
+        </Button>
       </Flex>
+
+      <AppText fontWeight="600" size="2xl">
+        Listen Now
+      </AppText>
+      <AppText c="gray.6">Top picks for you.</AppText>
+
+      <Box my={'md'} className={styles.topPick}>
+        {MAIN_MUSICS.map((music, index) => {
+          return (
+            <Paper
+              shadow="none"
+              key={music.title}
+              mr={index < MAIN_MUSICS.length ? 'md' : 0}
+            >
+              <Box className={styles.imageContainer} mb={'xs'}>
+                <Image
+                  radius={'md'}
+                  h={350}
+                  w={250}
+                  src={music.image}
+                  className={styles.image}
+                ></Image>
+              </Box>
+              <AppText fontWeight="600" size="sm">
+                {music.title}
+              </AppText>
+              <AppText c="gray.6" size="sm">
+                {music.name}
+              </AppText>
+            </Paper>
+          )
+        })}
+      </Box>
     </Box>
   )
 }
