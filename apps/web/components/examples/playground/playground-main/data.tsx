37393e5ebdb315d34cff9efc96a222f9fe89253a
Felix Lee <leeyoongti@gmail.com>
2024-02-03 11:20:44 +0800

    feat(playground): add model select

--- apps/web/components/examples/playground/playground-main/data.tsx
+++ apps/web/components/examples/playground/playground-main/data.tsx
@@ -20,3 +20,67 @@ export const MODES = [
     value: 'advanced',
   },
 ]
+
+export const MODELS = [
+  {
+    group: 'GPT-3',
+    items: [
+      {
+        label: 'text-davinci-003',
+        value: 'text-davinci-003',
+        description: {
+          main: 'Most capable GPT-3 model. Can do any task the other models can do, often with higher quality, longer output and better instruction-following. Also supports inserting completions within text.',
+          strengths:
+            'Complex intent, cause and effect, creative generation, search, summarization for audience',
+        },
+      },
+      {
+        label: 'text-curie-001',
+        value: 'text-curie-001',
+        description: {
+          main: 'Very capable, but faster and lower cost than Davinci.',
+          strengths:
+            'Language translation, complex classification, sentiment, summarization',
+        },
+      },
+      {
+        label: 'text-baggage-001',
+        value: 'text-baggage-001',
+        description: {
+          main: 'Capable of straightforward tasks, very fast, and lower cost.',
+          strengths: 'Moderate classification, semantic search',
+        },
+      },
+      {
+        label: 'text-ada-001',
+        value: 'text-ada-001',
+        description: {
+          main: 'Capable of very simple tasks, usually the fastest model in the GPT-3 series, and lowest cost.',
+          strengths:
+            'Parsing text, simple classification, address correction, keywords',
+        },
+      },
+    ],
+  },
+  {
+    group: 'Codex',
+    items: [
+      {
+        label: 'code-davinci-002',
+        value: 'code-davinci-002',
+        description: {
+          main: 'Almost as capable as Davinci Codex, but slightly faster. This speed advantage may make it preferable for real-time applications.',
+          strengths: 'Real-time application where low-latency is preferable',
+        },
+      },
+      {
+        label: 'code-cushman-001',
+        value: 'code-cushman-001',
+        description: {
+          main: 'Almost as capable as Davinci Codex, but slightly faster. This speed advantage may make it preferable for real-time applications.',
+          strengths: 'Real-time application where low-latency is preferable',
+        },
+      },
+    ],
+  },
+]
--- apps/web/components/examples/playground/playground-main/index.tsx
+++ apps/web/components/examples/playground/playground-main/index.tsx
@@ -1,18 +1,101 @@
-import { Box, Flex, SegmentedControl, Textarea } from "@mantine/core"
+'use client'
+
+import {
+  Box,
+  Combobox,
+  Flex,
+  HoverCard,
+  InputBase,
+  SegmentedControl,
+  Textarea,
+  useCombobox,
+} from '@mantine/core'
+import { AppSize, AppText } from '@the-one/ui'
+import { useState } from 'react'
+import { MODELS, MODES } from './data'
 import styles from './index.module.css'
-import { AppSize, AppText } from "@the-one/ui"
-import { MODES } from "./data"
 
 export const PlaygroundMain = () => {
+  const combobox = useCombobox({
+    onDropdownClose: () => combobox.resetSelectedOption(),
+  })
+  const [model, setModel] = useState('text-davinci-003')
+
   return (
     <Box p={'xs'} px={'md'}>
       <Flex>
         <Textarea flex={1} rows={30}></Textarea>
         <Box px={'sm'} className={styles.options}>
-          <AppText fontWeight="600" size="sm" mb={AppSize.sm}>Mode</AppText>
-          <SegmentedControl size="xs" fullWidth data={MODES}/>
+          <Box mb={'xs'}>
+            <AppText fontWeight="600" size="sm" mb={AppSize.sm}>
+              Mode
+            </AppText>
+            <SegmentedControl size="xs" fullWidth data={MODES} />
+          </Box>
 
-          <AppText fontWeight="600" size="sm" mb={AppSize.sm}>Model</AppText>
+          <Box mb={'xs'}>
+            <AppText fontWeight="600" size="sm" mb={AppSize.sm}>
+              Model
+            </AppText>
+            <Combobox
+              withinPortal={false}
+              store={combobox}
+              onOptionSubmit={(val) => {
+                setModel(val)
+                combobox.closeDropdown()
+              }}
+            >
+              <Combobox.Target>
+                <InputBase
+                  component="button"
+                  type="button"
+                  pointer
+                  rightSection={<Combobox.Chevron />}
+                  rightSectionPointerEvents="none"
+                  onClick={() => combobox.toggleDropdown()}
+                >
+                  {model}
+                </InputBase>
+              </Combobox.Target>
+
+              <Combobox.Dropdown>
+                <Combobox.Options>
+                  {MODELS.map((group) => {
+                    return (
+                      <Combobox.Group key={group.group} label={group.group}>
+                        {group.items.map((item) => {
+                          return (
+                            <HoverCard
+                              width={240}
+                              closeDelay={0}
+                              position="left-start"
+                              withinPortal={false}
+                              key={item.label}
+                            >
+                              <HoverCard.Target>
+                                <Combobox.Option value={item.value}>
+                                  {item.label}
+                                </Combobox.Option>
+                              </HoverCard.Target>
+                              <HoverCard.Dropdown>
+                                <Box>
+                                  <AppText fontWeight='600' mb={'xs'}>{item.label}</AppText>
+                                  <AppText c='dimmed' size='sm' mb={'lg'}>{item.description.main}</AppText>
+
+                                  <AppText size='sm' fontWeight='600'>Strengths</AppText>
+                                  <AppText c='dimmed' size='sm'>{item.description.strengths}</AppText>
+                                </Box>
+                              </HoverCard.Dropdown>
+                            </HoverCard>
+                          )
+                        })}
+                      </Combobox.Group>
+                    )
+                  })}
+                </Combobox.Options>
+              </Combobox.Dropdown>
+            </Combobox>
+          </Box>
         </Box>
       </Flex>
     </Box>
